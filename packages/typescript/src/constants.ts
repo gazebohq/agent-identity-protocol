88d5fd4f4b61ff5924e806844b09a4663fc5d8cb
ashconway <1097970+ashconway@users.noreply.github.com>
2026-09-02 09:21:46 +1000

    Add packages/typescript/src/constants.ts

---
+++ packages/typescript/src/constants.ts
@@ -0,0 +1,8 @@
+export const AIP_VERSION = "0.1" as const;
+export const AUDIT_LOG_VERSION = "1.0" as const;
+
+export const SCHEMA_URLS = {
+  identity: "https://schema.gazebohq.com/v0.1/identity.json",
+  policy: "https://schema.gazebohq.com/v0.1/policy.json",
+  auditLog: "https://schema.gazebohq.com/v0.1/audit-log.json",
+} as const;
